9b5c452190799717793f478b87dfae88677a79f0
user1 <user1@abc.com>
2021-11-11 10:10:55 +0000

    commit1

--- main.tf
+++ main.tf
@@ -1,2 +1,30 @@
-this is first file.
-updated.
+provider "google" {
+  project = var.project
+  region  = "us-central1"
+}
+
+
+
+variable "machine" {
+  type    = string
+  default = "f1-micro"
+}
+
+
+
+variable "project" {
+  type = string
+}
+resource "google_compute_instance" "myvm" {
+  name         = "myfirstvm"
+  machine_type = var.machine
+  zone         = "us-central1-a"
+  network_interface {
+    network = "default"
+  }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+}
